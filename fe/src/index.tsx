bed72ec89a6e2f1eeb51a5c95e1a607a839da624
SaeHan Kim / 김 세한 <coodie5712@gmail.com>
2020-11-29 03:49:02 +0900

    Add styling module and CSR
    Add styling module and CSR

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {BrowserRouter} from "react-router-dom";
+import {ThemeProvider} from "styled-components";
+
+import theme from "./schema/theme";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>  
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
